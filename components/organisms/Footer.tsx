fe0f1f19f82cc132c33ef8b0d55f5ce1e1011844
Khoirul Asfian <asfian99@gmail.com>
2022-04-15 14:25:56 +0700

    fix: fix footer on mobile

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
 
   return (
     <footer className="container mx-auto px-8 pt-4 pb-8 md:py-4 md:px-24 lg:px-36">
-      <div className="flex flex-row items-center justify-between">
+      <div className="flex flex-col items-center justify-center gap-3 md:flex-row md:justify-between">
         <p>All rights reserved &copy; Khoirul Asfian 2022</p>
 
         <div className="flex flex-row items-center justify-end gap-3">
